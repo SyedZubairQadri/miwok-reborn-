bf11342ac9df0628bba87ee88bae5b9ae8f80dcb
S.M Zubair <Devilhunter341@gmail.com>
2016-07-20 20:31:29 +0500

    going for phrase activity

--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -55,6 +56,16 @@ public class WordAdapter extends ArrayAdapter<Word> {
 
         // Return the whole list item layout (containing 2 TextViews and an ImageView)
         // so that it can be shown in the ListView
+
+        // Find the ImageView in the list_item.xml layout with the ID list_item_icon
+        ImageView imageView = (ImageView) listItemView.findViewById(R.id.abc);
+        // Get the image resource ID from the current AndroidFlavor object and
+        // set the image to iconView
+       imageView.setImageResource(currentWord.getImageResourceId()
+       );
+
+
+
         return listItemView;
     }
 }
